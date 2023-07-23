484a42d3fde351e7ed10559f79e2b4ff4e7fb3d2
bebossi <bernardobossi77@gmail.com>
2023-07-23 06:45:46 -0300

    creating store-modal form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
 "use client"
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetUpPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen()
+    }
+  }), [isOpen, onOpen]
+
   return(
      <div className="p-4">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}  >
-        Children
-      </Modal>
+        Root page
      </div>
      )
 };
