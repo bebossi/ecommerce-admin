d8bc74b221181f8a8abd80fdea837a15daf2081d
bebossi <bernardobossi77@gmail.com>
2023-07-27 10:08:23 -0300

    adding all funcionalities for products

---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,163 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const body = await req.json();
+    const { userId } = auth();
+
+    const {
+      name,
+      images,
+      categoryId,
+      colorId,
+      sizeId,
+      price,
+      isFeatured,
+      isArchived,
+    } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    if (!images || !images.length) {
+      return new NextResponse("images are required", { status: 400 });
+    }
+    if (!categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+    if (!colorId) {
+      return new NextResponse("Color id is required", { status: 400 });
+    }
+    if (!sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+    if (!params.productId) {
+      return new NextResponse("Billboard id is required", { status: 401 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    await prismadb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        name,
+        price,
+        categoryId,
+        sizeId,
+        colorId,
+        images: {
+          deleteMany: {},
+        },
+        isArchived,
+        isFeatured,
+      },
+    });
+
+    const product = await prismadb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (err) {
+    console.log("[PRODUCT_PATCH]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse("Product id is required", { status: 401 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const product = await prismadb.product.deleteMany({
+      where: {
+        id: params.productId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (err) {
+    console.log("[BILLBOARD_PATCH]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    if (!params.productId) {
+      return new NextResponse("Billboard id is required", { status: 401 });
+    }
+
+    const product = await prismadb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        images: true,
+        category: true,
+        color: true,
+        size: true,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (err) {
+    console.log("[PRODUCT_GET", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
